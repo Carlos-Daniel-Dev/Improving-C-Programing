4b2ab44f27b4e5656b4e2df0c504ec2c4cd356d8
= <carlosdaniel.programador@gmail.com>
2023-10-21 13:27:12 -0300

    bzero

---
+++ memory_alocation_and_manipulation/my_bzero.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * @brief Sets the first 'n' chars of a string 's' to zero ('\0').
+ * 
+ * @param s a string
+ * @param n amount of chars to be set to '\0'.
+ */
+
+void my_bzero(void *s, int n)
+{
+	unsigned int i;
+	char *c;
+
+	i = 0;
+
+	c = (char *) s;
+	while(i < n)
+	{
+		c[i] = '\0';
+		i++;
+	}
+
+	return ;
+
+}
